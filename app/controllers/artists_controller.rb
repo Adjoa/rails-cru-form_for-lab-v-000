e32d4b05af9d3f5b0736a68fd060853dcfa7b925
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-13 02:31:11 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,7 +5,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.create(artist_params(:name, :bio))
+    @artist = Artist.new(artist_params(:name, :bio))
     @artist.save
 
     redirect_to artist_path(@artist)
