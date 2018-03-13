a7213f6d25dda18ec9509c408c715a0c577e95b5
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-13 02:36:27 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,24 +1,35 @@
 class GenresController < ApplicationController
 
-  def index
-  end
-
   def new
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(artist_params(:name, :bio))
+    @genre.save
+
+    redirect_to artist_path(@genre)
   end
 
   def show
+    @genre = Genre.find(params[:id])
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(artist_params(:name, :bio))
+
+    redirect_to artist_path(@genre)
   end
 
-  def destroy
+  private
+
+  def artist_params(*args)
+    params.require(:genre).permit(*args)
   end
 
 end
